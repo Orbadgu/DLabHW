c1a6e9efc3154380a53403e126e8fed55697d20f
Bora <boradogru06@gmail.com>
2026-04-24 11:11:01 +0300

    Update dataset.py

--- 2ndHomeWork/src/dataset.py
+++ 2ndHomeWork/src/dataset.py
@@ -4,11 +4,10 @@ import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 
 def get_dataloaders(batch_size=64):
-    # Ön İşleme (Preprocessing)
     transform_train = transforms.Compose([
-        transforms.RandomCrop(32, padding=4),  # Görüntüyü hafifçe kaydırır
+        transforms.RandomCrop(32, padding=4),
         transforms.RandomHorizontalFlip(),
-        transforms.RandomRotation(15),  # 15 dereceye kadar rastgele döndürür
+        transforms.RandomRotation(15),
         transforms.ToTensor(),
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
     ])
@@ -18,7 +17,6 @@ def get_dataloaders(batch_size=64):
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
     ])
 
-    # CIFAR-10 Veri Seti
     trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
     trainloader = DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
 
